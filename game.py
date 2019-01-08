de5dcb0277f86994cfce1a61c44b15ddfa94c136
Your Name <you@example.com>
2019-01-08 12:45:21 -0800

    Added guessing

--- game.py
+++ game.py
@@ -7,4 +7,17 @@ print("{},I'm thinking of a number between 1 and 100.".format(name))
 print("Try to guess my number")
 secret_number = random.randint(1, 100)
 
+counter = 0
+guess = input("Your guess?")
 
+while guess != secret_number:
+    if guess > secret_number:
+        print("Your guess is too high, try again.")
+        counter += 1
+    elif guess < secret_number:
+        print("Your guess is too low, try again.")
+        counter += 1
+    else:
+        print("Well done, {}! You found my number in {} tries!".format(name, counter)
+
+ 
