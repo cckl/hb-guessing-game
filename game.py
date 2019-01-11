83c6a55ddc6876935c5c2c949e8ed3e2f2568fc9
Your Name <you@example.com>
2019-01-10 18:58:29 -0800

    finish game v1

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ print('\n')
 name = input('(type in your name) ')
 print("{},I'm thinking of a number between 1 and 100.".format(name))
 print("Try to guess my number")
+
 secret_number = random.randint(1, 100)
 
 counter = 0
@@ -16,22 +17,28 @@ while True:
         print("Please enter a valid integer!")
        
 
+def guessing_game(guess):
+    while True:
+        if int(guess) > 100 or int(guess) < 1:
+            print ('Sorry, please enter a number between 1 and 100!')
+            guess = int(input("Your guess? "))
 
-while True:
-    if int(guess) > 100 or int(guess) < 1:
-        print ('Sorry, please enter a number between 1 and 100!')
-        guess = int(input("Your guess? "))
+        elif guess > secret_number:
+            print("Your guess is too high, try again.")
+            counter += 1
+            guess = int(input("Your guess? "))
+        elif guess < secret_number:
+            print("Your guess is too low, try again.")
+            counter += 1
+            guess = int(input("Your guess? "))
+        else:
+            print("Well done {}! You found # in {} tries!".format(name, counter))
+            break
 
-    elif guess > secret_number:
-        print("Your guess is too high, try again.")
-        counter += 1
-        guess = int(input("Your guess? "))
-    elif guess < secret_number:
-        print("Your guess is too low, try again.")
-        counter += 1
-        guess = int(input("Your guess? "))
-    else:
-        print("Well done {}! You found # in {} tries!".format(name, counter))
-        break
+restart = input("Would you like to play again? Type Yes or No")
+if restart.lower() == "yes":
+
+
+
 
 
